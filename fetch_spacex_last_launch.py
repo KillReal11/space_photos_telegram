59bc359c57dbbb3d584f2be2bb0675aabe821b2e
Kirill <dawiduk2013@yandex.ru>
2026-05-19 21:06:58 +0500

    Create fetch_spacex_last_launch.py

---
+++ fetch_spacex_last_launch.py
@@ -0,0 +1,29 @@
+import requests
+from get_files_functions import download_photo
+from parse_inputs import parse_id
+from dotenv import load_dotenv
+import os
+
+
+def fetch_spacex_last_launch(proxy):
+    proxies = {'https': proxy}
+    spacex_id = parse_id()
+    spacex_url = f'https://api.spacexdata.com/v5/launches/{spacex_id}'
+    path = 'images/spacex'
+    response = requests.get(spacex_url, proxies=proxies, verify=False)
+    response.raise_for_status()
+    response_answer = response.json()
+    images = response_answer.get('links').get('flickr').get('original')
+    for number, image in enumerate(images):
+        filename = f'spacex{number}.jpg'
+        download_photo(image, path, filename)
+
+
+def main():
+    load_dotenv()
+    proxy = os.getenv("PROXY_SOCKS5")
+    fetch_spacex_last_launch(proxy=proxy)
+
+
+if __name__ == '__main__':
+    main()

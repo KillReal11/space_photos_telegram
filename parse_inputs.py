e9494e7475456938a01a5f3844823783e46f3e21
Kirill <dawiduk2013@yandex.ru>
2026-05-20 20:27:29 +0500

    Update parse_inputs.py

--- parse_inputs.py
+++ parse_inputs.py
@@ -3,36 +3,42 @@ from get_files_functions import get_random_file
 
 
 def parse_id():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
         'spacex_id',
         default='latest',
         nargs='?',
-        help='Введите id запуска spacex. По умолчанию: latest'
+        help='Введите id запуска spacex.\n'
+        'Пример id: 5eb87d47ffd86e000604b38a.\n'
+        'По умолчанию: %(default)s'
         )
     args = parser.parse_args()
     return args.spacex_id
 
 
 def parse_quantity():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
         'quantity',
         default='30',
         nargs='?',
-        help='Введите количество фотографий, которое хотите скачать. По умолчанию: 30'
+        help='Введите количество фотографий, которое хотите скачать.\n'
+        'Например: 12.\n'
+        'По умолчанию: %(default)s'
         )
     args = parser.parse_args()
     return args.quantity
 
 
 def parse_path():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
         'path',
         default=get_random_file(),
         nargs='?',
-        help='Введите путь фото. По умолчанию публикуется случайное фото'
+        help='Введите путь к фото. \n'
+        'Например: %(default)s.\n'
+        'По умолчанию публикуется случайное фото из папки images.'
         )
     args = parser.parse_args()
     return args.path
